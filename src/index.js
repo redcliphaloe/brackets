608935aea11d0b57ed00991c7892a2f4afbbb8a7
redcliphaloe <redcliphaloe@gmail.com>
2019-09-27 12:03:26 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = 
+function check(str, bracketsConfig) {
   // your solution
+  let counter = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    counter[i] = 0;      
+  }
+
+  let bracketType = [];
+  for (let i = 0; i < str.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+          if (str[i] === bracketsConfig[j][0]) {
+            if ((str[i] === bracketsConfig[j][0]) && (str[i] === bracketsConfig[j][1]) && (counter[j] === 1)) {
+              // 
+            } else {
+              bracketType[i] = 'open';
+              counter[j]++;               
+            }
+          }
+
+          if (str[i] === bracketsConfig[j][1]) {
+              if (counter[j] < 1) {
+                  return false;
+              }
+
+              if ((str[i] === bracketsConfig[j][0]) && (str[i] === bracketsConfig[j][1]) && (bracketType[i] === 'open')) {
+                // 
+              } else {
+                bracketType[i] = 'close';
+                counter[j]--;   
+                
+                if ((bracketType[i - 1] === 'open') && (str[i - 1] !== bracketsConfig[j][0])) {
+                  return false;
+                }                
+              }
+          }   
+      }      
+  }
+
+  return counter.join('') == 0;
 }
